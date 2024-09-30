7329ea3420e150b88a5f35463fedfc989d5be9f7
Salvador Pruneda <93486867+Spruneda134@users.noreply.github.com>
2024-09-30 23:38:22 +0000

    attendance

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,13 +1,46 @@
-fn concat_strings(s1: &String, s2: &String) -> String {
-    // Your code here
-    let mut word = s1.to_string();
-    word.push_str(s2);
-    return word;
+use std::fs::File;
+use std::io::{self, Read, Write};
+
+struct Car {
+    make: String,
+    model: String,
+    year: u32,
 }
 
-fn main() {
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("World!");
-    let result = concat_strings(&s1, &s2);
-    println!("{}", result); // Should print: "Hello, World!"
+fn reading_from_console() {
+    let mut buffer = String::new();
+
+    print!("What's your car make? ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let make = buffer.trim().to_string();
+
+    buffer.clear();
+
+    print!("What's your car model? ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let model = buffer.trim().to_string();
+
+    buffer.clear();
+
+    print!("what year is it? ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let year = buffer.trim().parse().unwrap();
+
+    let car = Car { make, model, year };
+
+    let mut file = File::create("user_info.txt").unwrap();
+    writeln!(file, "Your car is a {} {} {}!", car.year, car.make, car.model).unwrap();
+    println!("Your car is a {} {} {}!", car.year, car.make, car.model);
+
+    let mut file = File::open("user_info.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("File contents:\n{}", contents);
+}
+
+fn main(){
+    reading_from_console();
 }
