af58fa2da27029651628fd1f240929ee14e0896e
Salvador Pruneda <93486867+Spruneda134@users.noreply.github.com>
2024-10-05 20:05:26 +0000

    assignment05

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,46 +1,12 @@
-use std::fs::File;
-use std::io::{self, Read, Write};
+use std::process::Command;
 
-struct Car {
-    make: String,
-    model: String,
-    year: u32,
+fn executing_os_commands_linux() {
+    let output = Command::new("sudo cat example.txt")
+        .output()
+        .expect("Failed to execute command");
+
+    println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
 }
-
-fn reading_from_console() {
-    let mut buffer = String::new();
-
-    print!("What's your car make? ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut buffer).unwrap();
-    let make = buffer.trim().to_string();
-
-    buffer.clear();
-
-    print!("What's your car model? ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut buffer).unwrap();
-    let model = buffer.trim().to_string();
-
-    buffer.clear();
-
-    print!("what year is it? ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut buffer).unwrap();
-    let year = buffer.trim().parse().unwrap();
-
-    let car = Car { make, model, year };
-
-    let mut file = File::create("user_info.txt").unwrap();
-    writeln!(file, "Your car is a {} {} {}!", car.year, car.make, car.model).unwrap();
-    println!("Your car is a {} {} {}!", car.year, car.make, car.model);
-
-    let mut file = File::open("user_info.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    println!("File contents:\n{}", contents);
-}
-
-fn main(){
-    reading_from_console();
+fn main() {
+    executing_os_commands_linux();
 }
