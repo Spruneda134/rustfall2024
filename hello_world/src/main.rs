8259109ba7e035cc0a66eb273f8020ddcb7c383d
Salvador Pruneda <93486867+Spruneda134@users.noreply.github.com>
2024-10-07 17:52:41 +0000

    struct and match attendance verification

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,12 +1,46 @@
-use std::process::Command;
-
-fn executing_os_commands_linux() {
-    let output = Command::new("sudo cat example.txt")
-        .output()
-        .expect("Failed to execute command");
-
-    println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
+#[derive(Debug)]
+enum GradeLevel {
+    Bachelor,
+    Master,
+    PhD,
 }
+#[derive(Debug)]
+enum Major {
+    ComputerScience,
+    ElectricalEngineering,
+}
+#[derive(Debug)]
+struct Student {
+    name:String,
+    grade:GradeLevel,
+    major:Major
+}
+
+impl Student {
+    fn new(name:String,grade:GradeLevel,major:Major) -> Self {
+        Student {
+            name:name,
+            grade:grade,
+            major:major,
+        }
+    }
+
+    fn introduce_yourself(&self) {
+        //TODO! (implement printing info about Student
+        // use match statement)
+        match self.grade {
+            GradeLevel::Bachelor => println!("Hello, my name is {}, i am studying to get a {:?} in {:?}", self.name, self.grade, self.major),
+            GradeLevel::Master => println!("Hello, my name is {}, i am studying to get a {:?} in {:?}", self.name, self.grade, self.major),
+            GradeLevel::PhD => println!("Hello, my name is {}, i am studying to get a {:?} in {:?}", self.name, self.grade, self.major),
+        };
+        
+       
+        }
+    }
+
 fn main() {
-    executing_os_commands_linux();
+    let s1 = Student::new("Salvador".to_string(),
+    GradeLevel::Bachelor,
+    Major::ComputerScience);
+s1.introduce_yourself();
 }
